4220b0d2c266fbae45ddd9feac042f01aa69cabc
Yisheng Qiu <yishengq@student.unimelb.edu.au>
2018-03-03 07:35:16 +1100

    -Change the compiling approach according to cython-sphinx-example, which generates documents correctly.

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 #exts = cythonize([Extension("mag_calc", sources = ["mag_calc_cext.c", "mag_calc.pyx"])])
 
-exts = cythonize([Extension("magcalc", 
-                            sources = ["mag_calc_cext.c", "magcalc.pyx"],
-                            extra_compile_args=['-fopenmp'], 
-                            extra_link_args=['-fopenmp'])])
+exts = [Extension("magcalc", 
+                  sources = ["mag_calc_cext.c", "magcalc.pyx"],
+                  extra_compile_args=['-fopenmp'], 
+                  extra_link_args=['-fopenmp'])]
+
+for e in exts:
+    e.cython_directives = {"embedsignature":True}
 
 setup(name = 'magcalc',
       version = '0.2.4',
       description = 'Package to compute SEDs for a semi-analytic model',
       author = 'Yisheng Qiu',
       author_email = 'yishengq@student.unimelb.edu.au',
-      license = 'MIT',
       ext_modules = exts,
+      cmdclass = {'build_ext': build_ext},
       packages = ['filters'],
       package_data = {'filters':['*.npy']})
