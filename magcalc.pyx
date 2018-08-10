4f3ce29e71db3bfdd1742ed4395fd6c8dbf26b80
yqiuu <yishengq@student.unimelb.edu.au>
2018-08-10 16:10:07 +1000

    Simplify the code to get the IGM transmission

--- magcalc.pyx
+++ magcalc.pyx
@@ -680,159 +680,34 @@ cdef class stellar_population:
 # Functions to compute the IGM absorption                                       #
 #                                                                               #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-def Lyman_absorption_Fan(double[:] obsWaves, double z):
-    #=====================================================================
-    # Depreciate function. It is original to calculate the optical depth of
-    # Fan et al. 2006
-    #=====================================================================
+cdef extern from "clib/sector.h":
+    void add_Lyman_absorption(double *target, double *waves, int nWaves, double z)
+
+
+def Lyman_absorption(obsWaves, z):
+    """
+    Compute the IGM transmission curve from Inoue et al. 2014
+    
+    Parameters
+    ----------
+    obsWaves: array_like
+        Wavelengths in observer frames.
+    z: float
+        redshift.
+
+    Returns
+    -------
+    trans: ndarray
+        1-D array containing the IGM transmission.
+    """
+    obsWaves = np.array(obsWaves).flatten()
+    trans = np.ones(len(obsWaves))
     cdef:
-        int i
-        int nWaves = obsWaves.shape[0]
-        double[:] absorption = np.zeros(nWaves)
-        double tau
-        double ratio
-    for i in xrange(nWaves):
-        ratio = obsWaves[i]/1216.
-        if ratio < 1. + z:
-            if ratio < 6.5:
-                tau = .85*(ratio/5.)**4.3
-            else:
-                tau = .15*(ratio/5.)**10.9
-        else:
-            tau = 0.
-        absorption[i] = exp(-tau)
-
-    return np.asarray(absorption)
-
-
-DEF NLYMAN = 39 # Inoue calculated the absorption of 40th Lyman series
-
-def Lyman_absorption_Inoue(double[:] obsWaves, double z):
-    #=====================================================================
-    # Function to calculate the optical depth of Inoue et al. 2014
-    # It is called by galaxy_mags(...).
-    #
-    # obsWaves: wavelength in unit of angstrom
-    # z: redshift
-    #
-    # Return: transmission (dimensionless)
-    # Reference Inoue et al. 2014
-    #=====================================================================
-    cdef:
-        double LymanSeries[NLYMAN]
-        double LAF1[NLYMAN]
-        double LAF2[NLYMAN]
-        double LAF3[NLYMAN]
-        double DLA1[NLYMAN]
-        double DLA2[NLYMAN]
-
-    LymanSeries[:] = [1215.67, 1025.72, 972.537, 949.743, 937.803,
-                      930.748, 926.226, 923.150, 920.963, 919.352,
-                      918.129, 917.181, 916.429, 915.824, 915.329,
-                      914.919, 914.576, 914.286, 914.039, 913.826,
-                      913.641, 913.480, 913.339, 913.215, 913.104,
-                      913.006, 912.918, 912.839, 912.768, 912.703,
-                      912.645, 912.592, 912.543, 912.499, 912.458,
-                      912.420, 912.385, 912.353, 912.324]
-    LAF1[:] = [1.690e-02, 4.692e-03, 2.239e-03, 1.319e-03, 8.707e-04,
-               6.178e-04, 4.609e-04, 3.569e-04, 2.843e-04, 2.318e-04,
-               1.923e-04, 1.622e-04, 1.385e-04, 1.196e-04, 1.043e-04,
-               9.174e-05, 8.128e-05, 7.251e-05, 6.505e-05, 5.868e-05,
-               5.319e-05, 4.843e-05, 4.427e-05, 4.063e-05, 3.738e-05,
-               3.454e-05, 3.199e-05, 2.971e-05, 2.766e-05, 2.582e-05,
-               2.415e-05, 2.263e-05, 2.126e-05, 2.000e-05, 1.885e-05,
-               1.779e-05, 1.682e-05, 1.593e-05, 1.510e-05]
-    LAF2[:] = [2.354e-03, 6.536e-04, 3.119e-04, 1.837e-04, 1.213e-04,
-               8.606e-05, 6.421e-05, 4.971e-05, 3.960e-05, 3.229e-05,
-               2.679e-05, 2.259e-05, 1.929e-05, 1.666e-05, 1.453e-05,
-               1.278e-05, 1.132e-05, 1.010e-05, 9.062e-06, 8.174e-06,
-               7.409e-06, 6.746e-06, 6.167e-06, 5.660e-06, 5.207e-06,
-               4.811e-06, 4.456e-06, 4.139e-06, 3.853e-06, 3.596e-06,
-               3.364e-06, 3.153e-06, 2.961e-06, 2.785e-06, 2.625e-06,
-               2.479e-06, 2.343e-06, 2.219e-06, 2.103e-06]
-    LAF3[:] = [1.026e-04, 2.849e-05, 1.360e-05, 8.010e-06, 5.287e-06,
-               3.752e-06, 2.799e-06, 2.167e-06, 1.726e-06, 1.407e-06,
-               1.168e-06, 9.847e-07, 8.410e-07, 7.263e-07, 6.334e-07,
-               5.571e-07, 4.936e-07, 4.403e-07, 3.950e-07, 3.563e-07,
-               3.230e-07, 2.941e-07, 2.689e-07, 2.467e-07, 2.270e-07,
-               2.097e-07, 1.943e-07, 1.804e-07, 1.680e-07, 1.568e-07,
-               1.466e-07, 1.375e-07, 1.291e-07, 1.214e-07, 1.145e-07,
-               1.080e-07, 1.022e-07, 9.673e-08, 9.169e-08]
-    DLA1[:] = [1.617e-04, 1.545e-04, 1.498e-04, 1.460e-04, 1.429e-04,
-               1.402e-04, 1.377e-04, 1.355e-04, 1.335e-04, 1.316e-04,
-               1.298e-04, 1.281e-04, 1.265e-04, 1.250e-04, 1.236e-04,
-               1.222e-04, 1.209e-04, 1.197e-04, 1.185e-04, 1.173e-04,
-               1.162e-04, 1.151e-04, 1.140e-04, 1.130e-04, 1.120e-04,
-               1.110e-04, 1.101e-04, 1.091e-04, 1.082e-04, 1.073e-04,
-               1.065e-04, 1.056e-04, 1.048e-04, 1.040e-04, 1.032e-04,
-               1.024e-04, 1.017e-04, 1.009e-04, 1.002e-04]
-    DLA2[:] = [5.390e-05, 5.151e-05, 4.992e-05, 4.868e-05, 4.763e-05,
-               4.672e-05, 4.590e-05, 4.516e-05, 4.448e-05, 4.385e-05,
-               4.326e-05, 4.271e-05, 4.218e-05, 4.168e-05, 4.120e-05,
-               4.075e-05, 4.031e-05, 3.989e-05, 3.949e-05, 3.910e-05,
-               3.872e-05, 3.836e-05, 3.800e-05, 3.766e-05, 3.732e-05,
-               3.700e-05, 3.668e-05, 3.637e-05, 3.607e-05, 3.578e-05,
-               3.549e-05, 3.521e-05, 3.493e-05, 3.466e-05, 3.440e-05,
-               3.414e-05, 3.389e-05, 3.364e-05, 3.339e-05]
-
-    cdef:
-        int i, j
-        int nWaves = obsWaves.shape[0]
-        double[:] absorption = np.zeros(nWaves)
-        double tau
-        double lamObs, ratio
-
-    for i in xrange(nWaves):
-        tau = 0.
-        lamObs = obsWaves[i]
-        # Lyman series
-        for j in xrange(NLYMAN):
-            ratio = lamObs/LymanSeries[j]
-            if ratio < 1. + z:
-                # LAF terms
-                if ratio < 2.2:
-                    tau += LAF1[j]*ratio**1.2
-                elif ratio < 5.7:
-                    tau += LAF2[j]*ratio**3.7
-                else:
-                    tau += LAF3[j]*ratio**5.5
-                # DLA terms
-                if ratio < 3.:
-                    tau += DLA1[j]*ratio**2.
-                else:
-                    tau += DLA2[j]*ratio**3.
-        # Lyman continuum
-        ratio = lamObs/912.
-        # LAF terms
-        if z < 1.2:
-            if ratio < 1. + z:
-                tau += .325*(ratio**1.2 - (1. + z)**-.9*ratio**2.1)
-        elif z < 4.7:
-            if ratio < 2.2:
-                tau += 2.55e-2*(1. + z)**1.6*ratio**2.1 + .325*ratio**1.2 - .25*ratio**2.1
-            elif ratio < 1. + z:
-                tau += 2.55e-2*((1. + z)**1.6*ratio**2.1 - ratio**3.7)
-        else:
-            if ratio < 2.2:
-                tau += 5.22e-4*(1. + z)**3.4*ratio**2.1 + .325*ratio**1.2 - 3.14e-2*ratio**2.1
-            elif ratio < 5.7:
-                tau += 5.22e-4*(1. + z)**3.4*ratio**2.1 + .218*ratio**2.1 - 2.55e-2*ratio**3.7
-            elif ratio < 1. + z:
-                tau += 5.22e-4*((1. + z)**3.4*ratio**2.1 - ratio**5.5)
-        # DLA terms
-        if z < 2.:
-            if ratio < 1. + z:
-                tau += .211*(1. + z)**2. - 7.66e-2*(1. + z)**2.3*ratio**-.3 - .135*ratio**2.
-        else:
-            if ratio < 3.:
-                tau += .634 + 4.7e-2*(1. + z)**3. - 1.78e-2*(1. + z)**3.3*ratio**-.3 \
-                       -.135*ratio**2. - .291*ratio**-.3
-            elif ratio < 1. + z:
-                tau += 4.7e-2*(1. + z)**3. - 1.78e-2*(1. + z)**3.3*ratio**-.3 \
-                       -2.92e-2*ratio**3.
-        absorption[i] = exp(-tau)
-
-    return np.asarray(absorption)
+        double[::1] mvTrans = trans
+        double[::1] mvObsWaves = obsWaves
+        int nWaves = len(trans)
+    add_Lyman_absorption(&mvTrans[0], &mvObsWaves[0], nWaves, <double>z)
+    return trans
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
