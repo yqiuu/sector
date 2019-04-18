8d502ac0faa5ca6f4307a6d9a6633ea390e26dec
yqiuu <yishengq@student.unimelb.edu.au>
2019-04-18 11:02:03 +1000

    Move codes of Mason's dust model to dust.pyx

--- sector/dust/dust.pyx
+++ sector/dust/dust.pyx
@@ -2,7 +2,7 @@ from libc.math cimport exp
 from ..utils cimport *
 
 import numpy as np
-from numpy import isnan, vectorize
+from numpy import isnan, isscalar, vectorize
 
 
 __all__ = [
@@ -11,7 +11,9 @@ __all__ = [
     'SFR_model_params',
     'gas_model_params',
     'DTG_model_params',
-    'compute_mags_mhysa'
+    'compute_mags_mhysa',
+    'reddening_curve_Calzetti00',
+    'reddening_Mason15'
 ]
 
 
@@ -212,3 +214,148 @@ def compute_mags_mhysa(
     M1600 = -2.5*np.log10(flux[:, nBeta]) + 8.9
 
     return M1600, beta
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                               #
+# Dust model of Mason et al . 2015                                              #
+#                                                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+from warnings import warn
+
+from scipy.interpolate import interp1d
+from scipy.optimize import brentq
+
+
+DEF DUST_C = -2.33
+DEF DUST_M0 = -19.5
+DEF DUST_SIGMA = .34
+DEF DUST_BRIGHTER = -35.
+DEF DUST_FAINTER = 0.
+DEF DUST_BOUND = -5.
+
+
+cdef double beta_MUV(double obsMag, double slope, double inter):
+    if obsMag >= DUST_M0:
+        return (inter - DUST_C)*exp(slope*(obsMag - DUST_M0)/(inter - DUST_C)) \
+               + DUST_C
+    else:
+        return slope*(obsMag - DUST_M0) + inter
+
+
+cdef dust_equation(double obsMag, double slope, double inter,
+                   double insMag, double noise):
+    return obsMag - insMag \
+           - (4.43 + 1.99*(beta_MUV(obsMag, slope, inter) + noise))
+
+
+def dust_extinction(M1600, double z, double scatter):
+    #=====================================================================
+    # Calculate the dust extinction at rest frame 1600 angstrom
+    #
+    # M1600: rest frame 1600 angstrom magnitudes. It can be an array.
+    # z: redshift
+    #
+    # Returns: dust extinction at rest frame 1600 angstrom
+    #          M1600_obs = M1600 + A1600,
+    #          where M1600_obs is the dust attenuated magnitudes
+    # Reference Mason et al. 2015, equation 4
+    #           Bouwens 2014 et al. 2014, Table 3
+    #=====================================================================
+
+    cdef:
+        int iM
+        int nM
+    if isscalar(M1600):
+        nM = 1
+        M1600 = np.array([M1600], dtpye = 'f8')
+    else:
+        nM = len(M1600)
+        M1600 = np.asarray(M1600, dtype = 'f8')
+    cdef:
+        double insMag
+        double[:] mvM1600 = M1600
+        double[:] mvA1600 = np.zeros(nM)
+        double[:] mvScatter
+        double slope = interp1d([2.5, 3.8, 5., 5.9, 7., 8.],
+                                [-.2, -.11, -.14, -.2, -.2, -.15],
+                                fill_value = 'extrapolate')(z)
+        double inter = interp1d([2.5, 3.8, 5., 5.9, 7., 8.],
+                                [-1.7, -1.85, -1.91, -2., -2.05, -2.13],
+                                fill_value = 'extrapolate')(z)
+
+    if scatter != 0.:
+        mvScatter = np.random.normal(0., scatter, nM)
+        for iM in xrange(nM):
+            insMag = mvM1600[iM]
+            if insMag < DUST_BOUND:
+                mvA1600[iM] = brentq(dust_equation, DUST_BRIGHTER, DUST_FAINTER,
+                                     args = (slope, inter, mvM1600[iM], mvScatter[iM])) \
+                              - mvM1600[iM]
+            else:
+                mvA1600[iM] = 0.
+    else:
+        for iM in xrange(nM):
+            insMag = mvM1600[iM]
+            if insMag < DUST_BOUND:
+                mvA1600[iM] = brentq(dust_equation, DUST_BRIGHTER, DUST_FAINTER,
+                                     args = (slope, inter, mvM1600[iM], scatter)) \
+                              - mvM1600[iM]
+            else:
+                mvA1600[iM] = 0.
+    A1600 = np.asarray(mvA1600)
+    A1600[A1600 < 0.] = 0.
+    return A1600
+
+
+@vectorize
+def reddening_curve_Calzetti00(lam):
+    #=====================================================================
+    # Function of the reddening curve of Calzetti et al. 2000
+    #
+    # lam: wavelengths in a unit of angstrom
+    # Reference Calzetti et al. 2000, Liu et al. 2016
+    #=====================================================================
+    lam *= 1e-4 # Convert angstrom to mircometer
+    if lam < .12 or lam > 2.2:
+        warn("Warning: wavelength is beyond the range of the reddening curve")
+    if lam < .12:
+        return -92.44949*lam + 23.21331
+    elif lam < .63:
+        return 2.659*(-2.156 + 1.509/lam - 0.198/lam**2 + 0.011/lam**3) + 4.05
+    elif lam < 2.2:
+        return  2.659*(-1.857 + 1.040/lam) + 4.05
+    else:
+        return max(0., -.57136*lam + 1.62620)
+
+
+def reddening_Mason15(waves, M1600, z, scatter = 0.):
+    """
+    Compute the dust extinction at given wavelengths.
+
+    Parameters
+    ----------
+    waves: array_like
+        Wavelength in a unit of :math:`\\unicode{x212B}`.
+    M1600: array_like
+        Magnitudes at rest-frame 1600 :math:`\\unicode{x212B}`.
+    z: float
+        redshift.
+    scatter: float
+        Add a Gaussian scatter to the Meurer relation. If 0, no
+        scatter is applied.
+
+    Returns
+    -------
+    A: array_like
+        Dust extinction at given wavelengths, which is additive to AB
+        magnitudes. It has a dimension of ``(len(M1600), len(waves))``.
+    """
+    A1600 = dust_extinction(M1600, z, scatter)
+    reddening = reddening_curve_Calzetti00(waves)/reddening_curve_Calzetti00(1600.)
+    #
+    waves = np.ravel(waves)
+    if len(waves) == 1:
+        return reddening*A1600
+    else:
+        return reddening*A1600.reshape(-1, 1)
--- sector/sector.pyx
+++ sector/sector.pyx
@@ -5,8 +5,6 @@ from utils cimport *
 from sfh cimport *
 
 import os, sys
-from warnings import warn
-from copy import deepcopy
 
 import numpy as np, h5py
 from numpy import isscalar
@@ -24,9 +22,6 @@ __all__ = [
     'beta_filters',
     'composite_spectra',
     'calibration',
-    'dust_extinction',
-    'reddening_curve',
-    'reddening'
 ]
 
 
@@ -679,146 +674,3 @@ cdef class calibration:
 
 def rebuild_calibration(sfhList, sedPath, betaBands, approx, nThread):
     return calibration(sfhList, sedPath, betaBands, approx, nThread)
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                                                                               #
-# Dust model of Mason et al . 2015                                              #
-#                                                                               #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-from numpy import vectorize
-from scipy.interpolate import interp1d
-from scipy.optimize import brentq
-
-DEF DUST_C = -2.33
-DEF DUST_M0 = -19.5
-DEF DUST_SIGMA = .34
-DEF DUST_BRIGHTER = -35.
-DEF DUST_FAINTER = 0.
-DEF DUST_BOUND = -5.
-
-
-cdef double beta_MUV(double obsMag, double slope, double inter):
-    if obsMag >= DUST_M0:
-        return (inter - DUST_C)*exp(slope*(obsMag - DUST_M0)/(inter - DUST_C)) \
-               + DUST_C
-    else:
-        return slope*(obsMag - DUST_M0) + inter
-
-
-cdef dust_equation(double obsMag, double slope, double inter,
-                   double insMag, double noise):
-    return obsMag - insMag \
-           - (4.43 + 1.99*(beta_MUV(obsMag, slope, inter) + noise))
-
-
-def dust_extinction(M1600, double z, double scatter):
-    #=====================================================================
-    # Calculate the dust extinction at rest frame 1600 angstrom
-    #
-    # M1600: rest frame 1600 angstrom magnitudes. It can be an array.
-    # z: redshift
-    #
-    # Returns: dust extinction at rest frame 1600 angstrom
-    #          M1600_obs = M1600 + A1600,
-    #          where M1600_obs is the dust attenuated magnitudes
-    # Reference Mason et al. 2015, equation 4
-    #           Bouwens 2014 et al. 2014, Table 3
-    #=====================================================================
-
-    cdef:
-        int iM
-        int nM
-    if isscalar(M1600):
-        nM = 1
-        M1600 = np.array([M1600], dtpye = 'f8')
-    else:
-        nM = len(M1600)
-        M1600 = np.asarray(M1600, dtype = 'f8')
-    cdef:
-        double insMag
-        double[:] mvM1600 = M1600
-        double[:] mvA1600 = np.zeros(nM)
-        double[:] mvScatter
-        double slope = interp1d([2.5, 3.8, 5., 5.9, 7., 8.],
-                                [-.2, -.11, -.14, -.2, -.2, -.15],
-                                fill_value = 'extrapolate')(z)
-        double inter = interp1d([2.5, 3.8, 5., 5.9, 7., 8.],
-                                [-1.7, -1.85, -1.91, -2., -2.05, -2.13],
-                                fill_value = 'extrapolate')(z)
-
-    if scatter != 0.:
-        mvScatter = np.random.normal(0., scatter, nM)
-        for iM in xrange(nM):
-            insMag = mvM1600[iM]
-            if insMag < DUST_BOUND:
-                mvA1600[iM] = brentq(dust_equation, DUST_BRIGHTER, DUST_FAINTER,
-                                     args = (slope, inter, mvM1600[iM], mvScatter[iM])) \
-                              - mvM1600[iM]
-            else:
-                mvA1600[iM] = 0.
-    else:
-        for iM in xrange(nM):
-            insMag = mvM1600[iM]
-            if insMag < DUST_BOUND:
-                mvA1600[iM] = brentq(dust_equation, DUST_BRIGHTER, DUST_FAINTER,
-                                     args = (slope, inter, mvM1600[iM], scatter)) \
-                              - mvM1600[iM]
-            else:
-                mvA1600[iM] = 0.
-    A1600 = np.asarray(mvA1600)
-    A1600[A1600 < 0.] = 0.
-    return A1600
-
-
-@vectorize
-def reddening_curve(lam):
-    #=====================================================================
-    # Function of the reddening curve of Calzetti et al. 2000
-    #
-    # lam: wavelengths in a unit of angstrom
-    # Reference Calzetti et al. 2000, Liu et al. 2016
-    #=====================================================================
-    lam *= 1e-4 # Convert angstrom to mircometer
-    if lam < .12 or lam > 2.2:
-        warn("Warning: wavelength is beyond the range of the reddening curve")
-    if lam < .12:
-        return -92.44949*lam + 23.21331
-    elif lam < .63:
-        return 2.659*(-2.156 + 1.509/lam - 0.198/lam**2 + 0.011/lam**3) + 4.05
-    elif lam < 2.2:
-        return  2.659*(-1.857 + 1.040/lam) + 4.05
-    else:
-        return max(0., -.57136*lam + 1.62620)
-
-
-def reddening(waves, M1600, z, scatter = 0.):
-    """
-    Compute the dust extinction at given wavelengths.
-
-    Parameters
-    ----------
-    waves: array_like
-        Wavelength in a unit of :math:`\\unicode{x212B}`.
-    M1600: array_like
-        Magnitudes at rest-frame 1600 :math:`\\unicode{x212B}`.
-    z: float
-        redshift.
-    scatter: float
-        Add a Gaussian scatter to the Meurer relation. If 0, no
-        scatter is applied.
-
-    Returns
-    -------
-    A: array_like
-        Dust extinction at given wavelengths, which is additive to AB
-        magnitudes. It has a dimension of ``(len(M1600), len(waves))``.
-    """
-    A1600 = dust_extinction(M1600, z, scatter)
-    if isscalar(waves):
-        return reddening_curve(waves)/reddening_curve(1600.)*A1600
-    else:
-        waves = np.asarray(waves)
-        return reddening_curve(waves)/reddening_curve(1600.)*A1600.reshape(-1, 1)
-
-
